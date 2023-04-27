3f518db246acd7d66d668b5f5ca43d04db485d99
Dragana Spasojevic <draganaspasojevic@Draganas-Air.fritz.box>
2023-04-27 10:14:29 +0200

    Added script for installing java on ec2 instance, and modified launchtemplate and autoscaling group

--- backend_app.sh
+++ backend_app.sh
@@ -1,118 +1,16 @@
 #!/bin/bash
 
-mkdir /home/ec2-user/jars >>EOF
+mkdir /home/ec2-user/jars
 
-# INSTANCE_ID="ami-0df24e148fdb9f1d8"
+sudo chmod 777 jars/
 
-# # SSH key file for authentication
-# SSH_KEY=/Downloads/labsuser.pem
+cd /home/ec2-user/jars/
 
-# # Path to the .jar file on my machine
-# FILE_PATH=/Documents/dentaloffice/dental-office-api/build/libs/dentaloffice-0.0.1-SNAPSHOT.jar
+sudo yum update -y
 
-# mkdir jars
+sleep 30s
 
-# DEST_PATH=/home/ec2-user/jars
+sudo amazon-linux-extras install java-openjdk11
 
-# cd jars
-
-# aws ec2 scp $FILE_PATH ec2-user@$INSTANCE_ID:$DEST_PATH -i $SSH_KEY
-
-# sudo yum update
-# sudo yum install java
-
-# java -jar dentaloffice-o.0..1-SNAPSHOT.jar
-
-
-
-# # Update the package repositories
-# sudo yum update u-y
-
-# # Install Git
-# sudo yum install -y git
-
-# # Clone my project repository
-# git clone git@gitlab.com:d5911/dental-office-api.git
-
-# # Navigate to the project directory
-# cd /Documents/dentaloffice/dental-office-api
-
-# # Make the Gradle wrapper executable
-# chmod +x gradlew
-
-# # Build the project
-# ./gradlew build
-
-# # Path to the .jar file on my machine
-# jar_file=/Documents/dentaloffice/dental-office-api/build/libs/dentaloffice-0.0.1-SNAPSHOT.jar
-
-# # SSH key file for authentication
-# ssh_key=/Downloads/labsuser.pem
-
-# # IP address or host name of the EC2 instance
-# ec2_instance=ip-10-0-4-203.us-west-2.compute.internal
-
-# EC2_USERNAME=ec2-user
-
-# # Install OpenJDK 11
-# ssh -i $ssh_key $EC2_USERNAME@$ec2_instance << EOF
-# sudo yum -y update
-# sudo yum -y install java-11-openjdk-devel
-# exit
-# EOF
-
-# # Destination directory on the EC2 instance
-# destination_dir=/home/ec2-user/
-
-
-
-# # Copy the JAR file to the EC2 instance
-# scp -i $ssh_key $jar_file $EC2_USERNAME@$ec2_instance:/home/$EC2_USERNAME/$jar_file
-
-# # SSH into the EC2 instance and run the application
-# ssh -i $ssh_key $EC2_USERNAME@$ec2_instance << EOF
-# cd /home/$EC2_USERNAME
-# java -jar $jar_file &
-# exit
-# EOF
-
-echo "Deployment complete."
-
-# # Use scp to copy the .jar file to the EC2 instance
-# scp -i $ssh_key $jar_file $ec2_instance:$destination_dir
-
-
-
-
-
-# #!/bin/bash
-
-# # Set variables
-# APP_NAME=<dental-office-api>
-# EC2_HOST=<your_ec2_instance_public_ip>
-# EC2_USERNAME=<your_ec2_instance_username>
-# KEY_PATH=<your_ssh_key_path>
-# JAR_FILE=<your_jar_file_name>
-
-# # Install OpenJDK 11
-# ssh -i $KEY_PATH $EC2_USERNAME@$EC2_HOST << EOF
-# sudo yum -y update
-# sudo yum -y install java-11-openjdk-devel
-# exit
-# EOF
-
-# # Build the JAR file with Gradle
-# ./gradlew clean build
-
-# # Copy the JAR file to the EC2 instance
-# scp -i $KEY_PATH build/libs/$JAR_FILE $EC2_USERNAME@$EC2_HOST:/home/$EC2_USERNAME/$APP_NAME.jar
-
-# # SSH into the EC2 instance and run the application
-# ssh -i $KEY_PATH $EC2_USERNAME@$EC2_HOST << EOF
-# cd /home/$EC2_USERNAME
-# java -jar $APP_NAME.jar &
-# exit
-# EOF
-
-# echo "Deployment complete."
+sudo yum install java-11-openjdk-devel
 
--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,22 +1,13 @@
-# resource "local_file" "backend_app_script" {
-#   filename = "/backend_app.sh"
-#   mode = "0755"
-  
-# }
 # Launch Template Resource
-resource "aws_launch_template" "launchtemplate_back2" {
-  name = "launchtemplate_back2"
+resource "aws_launch_template" "launchtemplate_back4" {
+  name     = "launchtemplate_back4"
   image_id = "ami-0df24e148fdb9f1d8"
   # image_id = "ami-0747e613a2a1ff483"
   instance_type = "t2.micro"
   # iam_instance_profile = "arn:aws:iam::414207150251:instance-profile/LabInstanceProfile"
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
-  key_name = "vockey"
-  # user_data = base64encode(file("${path.module}/backend_app.sh"))
-  user_data = <<-EOF 
-              #!/bin/bash
-              mkdir /home/ec2-user/jars
-              EOF
+  key_name               = "vockey"
+  user_data = base64encode(file("${path.module}/backend_app.sh"))                                                         
   monitoring {
     enabled = true
   }
@@ -27,3 +18,4 @@ resource "aws_launch_template" "launchtemplate_back2" {
     }
   }
 }
+                                                                                                      
