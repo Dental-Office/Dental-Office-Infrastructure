5c62b2a81ac6f90ed710873fda628bf30e1839dd
Dragana Spasojevic <draganaspasojevic@Draganas-Air.fritz.box>
2023-05-02 14:06:50 +0200

    created highly available data base

--- backend_app.sh
+++ backend_app.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-
-
 # sudo amazon-linux-extras install java-openjdk11
+# sudo yum install java-17-amazon-corretto.x86_64
 # sudo yum install java-17-amazon-corretto-devel
-# sudo yum install java-11-openjdk-devel
 
 mkdir /home/ec2-user/jars
 
 sudo chmod 777 /home/ec2-user/jars/
 
-# cd /home/ec2-user/jars/
-
 sudo yum update -y
 
-sudo yum install java-17-amazon-corretto-devel
-
-
+sudo yum install java-17-amazon-corretto-devel -y
--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,12 +1,13 @@
 # Launch Template Resource
-resource "aws_launch_template" "launchtemplate_back7" {
-  name     = "launchtemplate_back7"
-  image_id = "ami-0df24e148fdb9f1d8"
-  # image_id = "ami-0747e613a2a1ff483"
+resource "aws_launch_template" "launchtemplate_back9" {
+  name     = "launchtemplate_back9"
+  # image_id = "ami-0df24e148fdb9f1d8"
+  image_id = "ami-009c5f630e96948cb"
   instance_type = "t2.micro"
   # iam_instance_profile = "arn:aws:iam::414207150251:instance-profile/LabInstanceProfile"
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
   key_name               = "vockey"
+  # update_default_version = ture
   user_data = base64encode(file("${path.module}/backend_app.sh"))                                                         
   monitoring {
     enabled = true
